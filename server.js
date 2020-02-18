2358ee7550cb65b7fd3cb718e4119c2ef7ab059f
Jerry Cheruvathoor <jerry.cheruvathoor@nike.com>
2020-02-17 19:58:24 -0800

    added code for data engineering blog

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+const path = require('path');
+ 
+const express = require('express');
+ 
+const app = new express();
+ 
+app.use(express.static('public'));
+ 
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+});
+ 
+app.listen(80, () => {
+    console.log('App listening on port 80')
+});
